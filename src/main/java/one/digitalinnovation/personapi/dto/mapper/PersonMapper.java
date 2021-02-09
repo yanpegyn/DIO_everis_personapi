5fc8252aa23e10ef04bb147d53558c3553627734
Yan Gustavo Pegyn Silva <46832714+yanpegyn@users.noreply.github.com>
2021-02-09 20:24:11 -0300

    Crash Fix

--- src/main/java/one/digitalinnovation/personapi/dto/mapper/PersonMapper.java
+++ src/main/java/one/digitalinnovation/personapi/dto/mapper/PersonMapper.java
@@ -7,6 +7,7 @@ import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface PersonMapper {
+
     @Mapping(target = "birthDate", source = "birthDate", dateFormat = "dd-MM-yyyy")
     Person toModel(PersonDTO dto);
 
